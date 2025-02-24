fc779692053aa6acbbf56239201c24a351fcfe87
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-02-24 09:10:43 -0800

    Modified structure and separated files

---
+++ app.ts
@@ -0,0 +1,12 @@
+// import { setupSearch } from "./components/Search";
+// import { openModal, closeModal } from "./components/Modal";
+// import { addTask, deleteTask, tasks } from "./components/TaskManager";
+
+// setupSearch(tasks);
+
+// document.getElementById("addTaskBtn")?.addEventListener("click", () => {
+//   addTask("add");
+// });
+
+// document.getElementById("openModalBtn")?.addEventListener("click", () => openModal("taskModal"));
+// document.getElementById("closeModalBtn")?.addEventListener("click", () => closeModal("taskModal"));
